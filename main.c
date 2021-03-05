1d5af81cc4796c7b78943e3f616cdace5c1b2f30
huloves <17791627852@163.com>
2021-03-05 22:51:02 +0800

    died_task_schedule and context_swap

--- main.c
+++ main.c
@@ -8,7 +8,8 @@ void test(void* args)
 {
     // while(1) {
         // sleep(1);
-        printf("AAAAAAAA\n");
+        // printf("A, a = %d\n", a++);
+        printf("AAAAAAAAAAAA\n");
         // task_exit(current_task);
         // task_exit(current_task);
         // pause();
@@ -18,8 +19,9 @@ void test(void* args)
 void test1(void* args)
 {
     while(1) {
-        sleep(1);
-        printf("BBBBBBBB\n");
+        // sleep(1);
+        // printf("B, a = %d\n", a++);
+        printf("BBBBBBBBBBBB\n");
         // task_exit(current_task);
         // pause();
     }
@@ -43,8 +45,8 @@ int main()
     interrupt_enable();
     while(1) {
         sleep(1);
-        printf("MMMMMMMM\n");
-        // pause();
+        // printf("MMMMMMMM\n");
+        pause();
     }
     return 0;
 }
--- task.c
+++ task.c
@@ -29,15 +29,14 @@ struct task_struct* main_task;   //主任务tcb
 struct task_struct* current_task;   //记录当前任务
 struct list task_ready_list;
 struct list task_all_list;
+struct list task_died_list;
 
 static struct list_elem* task_tag;   //保存队列中的任务节点
+static void died_task_schedule();
 
-/**
- * switch_to - 任务切换
- * **/
-static void switch_to_next(struct task_struct* next);
+extern void context_swap(struct sigcontext* context);
 
-/**
+/*
  * task_entrance - 执行任务函数function(func_arg)
  * @function: 任务处理函数
  * @func_arg: 任务参数
@@ -46,7 +45,6 @@ static void task_entrance(task_func* function, void* func_arg)
 {
     function(func_arg);
     task_exit(current_task);
-    while(1);
 }
 
 /**
@@ -55,6 +53,7 @@ static void task_entrance(task_func* function, void* func_arg)
  * **/
 void task_exit(struct task_struct* task)
 {
+    //关闭中断
     interrupt_disable();
 
     task->status = TASK_DIED;
@@ -72,7 +71,14 @@ void task_exit(struct task_struct* task)
         free(task);
     }
 
+    //打开中断
     interrupt_enable();
+
+    //任务空转，等待下次时钟信号
+    // while(1);
+
+    //上下文转换为next的上下文
+    died_task_schedule();
 }
 
 /**
@@ -94,9 +100,11 @@ void init_task(struct task_struct* ptask, char* name, int prio)
     }
 
     //task_stack指向栈顶
-    uint8_t* stack_min_addr = (uint8_t*)malloc(TASK_STACK_SIZE);
-    ptask->stack_min_addr = stack_min_addr;
-    ptask->task_stack = (uint64_t*)(stack_min_addr + TASK_STACK_SIZE);
+    // uint8_t* stack_min_addr = (uint8_t*)malloc(TASK_STACK_SIZE);
+    ptask->stack_min_addr = (uint8_t*)malloc(TASK_STACK_SIZE);
+    // ptask->stack_min_addr = stack_min_addr;
+    // ptask->task_stack = (uint64_t*)(stack_min_addr + TASK_STACK_SIZE);
+    ptask->task_stack = (uint64_t*)(ptask->stack_min_addr + TASK_STACK_SIZE);
 
     ptask->priority = prio;
     ptask->ticks = prio;
@@ -182,6 +190,7 @@ static void task_create(struct task_struct* ptask, task_func function, void* fun
  * **/
 struct task_struct* task_start(char* name, int prio, task_func function, void* func_arg)
 {
+    interrupt_disable();
     struct task_struct* task = (struct task_struct*)malloc(sizeof(struct task_struct));
 
     init_task(task, name, prio);
@@ -197,6 +206,7 @@ struct task_struct* task_start(char* name, int prio, task_func function, void* f
     //加入到全部任务队列
     list_append(&task_all_list, &task->all_list_tag);
 
+    interrupt_enable();
     return task;
 }
 
@@ -254,6 +264,7 @@ void task_init(void)
     printf("task_init start.\n");
     list_init(&task_ready_list);
     list_init(&task_all_list);
+    list_init(&task_died_list);
     
     //将当前main函数创建为任务
     make_main_task();
@@ -335,4 +346,28 @@ static void switch_to_next(struct task_struct* next)
     
     current_task = next;
     siglongjmp(next->env, 1);
+}
+
+/**
+ * died_task_schedule - 死亡任务主动让出CPU
+ * **/
+static void died_task_schedule()
+{
+    //屏蔽信号，退出后要打开信号
+    
+    //获取下一个要调度的任务
+    if(list_empty(&task_ready_list)) {
+        printf("task_ready_list is empty!\n");
+        while(1);
+    }
+
+    assert(!list_empty(&task_ready_list));
+    task_tag = NULL;
+    task_tag = list_pop(&task_ready_list);
+    struct task_struct* next = elem2entry(struct task_struct, general_tag, task_tag);
+    next->status = TASK_RUNNING;
+
+    //进行上下文切换，将上下文切换为要执行任务的上下文
+    current_task = next;
+    context_swap(&current_task->context);
 }
