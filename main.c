4891851ba12d702c478cb97f45e7f10953f1bc91
ZheNing Hu <adlternative@gmail.com>
2021-04-18 21:52:11 +0800

    修改 main 然后跑到一半就报错了
    maiN maiN maiN argA @1804289383@
    argA @1804289383@
    argA @1804289383@
    argA @1804289383@
    argA @1804289383@
    argA @1804289383@
    argA @1804289383@
    argA @1804289383@
    argA @1804289383@
    argA @1804289383@
    argA @1804289383@
    argA @1804289383@
    argA @1804289383@
    argA @1804289383@
    argA @1804289383@
    argA @1804289383@
    argA @1804289383@
    argA @1804289383@
    argA @1804289383@
    Timer expire! Jiffies: 265488427
    argA @1804289383@
    
    filename interrupt/timer.c
    line 321
    function interrupt_timer_handler
    condition current_task->stack_magic == 0x19991120
    
    Bug-report-by: ZheNing Hu <adlternative@gmail.com>

--- main.c
+++ main.c
@@ -3,27 +3,55 @@
 #include "init.h"
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+
 
 void test(void* args)
 {
     char* str = args;
-    console_put_str(str);
-}
+    char s[20];
+    snprintf(s,20,"@%d@\n",rand());
+    while(1) {
+        sleep(1);
+        console_put_str(str);
+        console_put_str(s);
+    }}
 
 void test1(void* args)
 {
     char* str = args;
+    char s[20];
+    snprintf(s,20,"#%d#\n",rand());
     while(1) {
         sleep(1);
         console_put_str(str);
+        console_put_str(s);
     }
+}
+
+/* cpu密集 */
+void test2(void* args)
+{
+  while(1);
+}
+
+/* io密集 */
+void test3(void* args)
+{
+  while(1){
+        console_put_str("rrr\n");
+  }
 }
 
 int main()
 {
     init();
+
     task_start("tast1", 31, test, "argA ");
-    for(int i = 0; i < 100; i++) {
+    task_start("tast2", 31, test2, "test2");
+    task_start("tast3", 31, test3, "test3");
+
+    for(int i = 0; i < 10; i++) {
         task_start("abc", 31, test1, "a ");
     }
     while(1) {
